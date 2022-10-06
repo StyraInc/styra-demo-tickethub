6a27be09c9b7aba0efcb556adbe21e2da9f6f67e
wusticality <1028792+wusticality@users.noreply.github.com>
2022-10-06 15:12:46 -0500

    add tickethub node

---
+++ backend/node/src/api.js
@@ -0,0 +1,45 @@
+import {Router} from "express";
+import 'express-async-errors'; // makes sure uncaught errors in async handlers doesn't cause crash
+import StatusCodes from "http-status-codes";
+import {tickets as ticketsDb, users as usersDb} from './db.js';
+
+export const router = Router()
+const {OK, FORBIDDEN} = StatusCodes
+
+/* Resolve a ticket. */
+router.post('/tickets/:id/resolve', async (req, res) => {
+  const ticket = ticketsDb[req.auth.tenant][req.params.id]
+  ticket.resolved = req.body.resolved ? true : false
+  ticket.last_updated = now()
+  return res.status(OK).json(asTicket(req.params.id, ticket))
+})
+
+/* Create tickets. */
+router.post('/tickets', async (req, res) => {
+  const id = ticketsDb[req.auth.tenant].length
+  const ticket = req.body
+  ticket.last_updated = now()
+  ticketsDb[req.auth.tenant].push(ticket)
+  return res.status(OK).json(asTicket(id, ticket))
+})
+
+/* List all tickets. */
+router.get('/tickets', async (req, res) => {
+  const tickets = ticketsDb[req.auth.tenant]
+    .map((t, index) => asTicket(index, t))
+  return res.status(OK).json({tickets})
+})
+
+/* Get a ticket. */
+router.get('/tickets/:id', async (req, res) => {
+  const ticket = ticketsDb[req.auth.tenant][req.params.id]
+  return res.status(OK).json(asTicket(req.params.id, ticket))
+})
+
+function now() {
+  return new Date().toISOString().split('.')[0] + 'Z'
+}
+
+function asTicket(id, attrs) {
+  return {id, ...attrs}
+}
