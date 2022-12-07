0d0d3f406d9d8fc7776de6eb32ecb1ca8d92d497
Johan Fylling <johan.dev@fylling.se>
2022-12-07 19:07:32 +0100

    Adding Java back-end integration
    Signed-off-by: Johan Fylling <johan.dev@fylling.se>

--- server/java/src/main/java/com/styra/tickethub/TicketHub.java
+++ server/java/src/main/java/com/styra/tickethub/TicketHub.java
@@ -1,10 +1,17 @@
 package com.styra.tickethub;
 
+import com.styra.run.Input;
+import com.styra.run.StyraRun;
+import com.styra.run.servlet.ProxyServlet;
+import com.styra.run.servlet.rbac.RbacServletHelper;
+import com.styra.run.servlet.session.TenantSessionManager;
+import com.styra.run.session.TenantInputTransformer;
 import com.styra.tickethub.Storage.Ticket;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotAllowedException;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -14,6 +21,7 @@ import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.servlet.ServletContainer;
 
 import java.util.Arrays;
@@ -30,6 +38,11 @@ public class TicketHub {
     }
 
     private static final Storage storage = Storage.create();
+    private static final StyraRun styraRun = StyraRun.builder(
+                    System.getenv("STYRA_URL"),
+                    System.getenv("STYRA_TOKEN"))
+            .build();
+    private static final TenantSessionManager sessionManager = new TenantSessionManager(TicketHub::getSessionAttributes);
 
     private @Context
     HttpServletRequest request;
@@ -38,6 +51,10 @@ public class TicketHub {
     @Path("/tickets")
     @Produces({MediaType.APPLICATION_JSON})
     public Tickets getTickets() {
+        var allowed = styraRun.check("/tickets/read/allow", new Input<>(getSessionAttributes())).join();
+        if (!allowed) {
+            throw new NotAllowedException("Not authorized");
+        }
         return new Tickets(storage.getTickets(getTenant()));
     }
 
@@ -45,6 +62,10 @@ public class TicketHub {
     @Path("/tickets/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Ticket getTicket(@PathParam("id") int id) {
+        var allowed = styraRun.check("/tickets/read/allow", new Input<>(getSessionAttributes())).join();
+        if (!allowed) {
+            throw new NotAllowedException("Not authorized");
+        }
         var tickets = storage.getTickets(getTenant());
         var ticket = tickets.get(id);
         if (ticket == null) {
@@ -58,6 +79,10 @@ public class TicketHub {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Ticket addTicket(Ticket ticket) {
+        var allowed = styraRun.check("/tickets/create/allow", new Input<>(getSessionAttributes())).join();
+        if (!allowed) {
+            throw new NotAllowedException("Not authorized");
+        }
         return storage.addTicket(getTenant(), ticket);
     }
 
@@ -65,6 +90,10 @@ public class TicketHub {
     @Path("/tickets/{id}/resolve")
     @Produces(MediaType.APPLICATION_JSON)
     public void resolveTicket(@PathParam("id") int id, TicketStatus status) {
+        var allowed = styraRun.check("/tickets/resolve/allow", new Input<>(getSessionAttributes())).join();
+        if (!allowed) {
+            throw new NotAllowedException("Not authorized");
+        }
         var tickets = storage.getTickets(getTenant());
         var ticket = tickets.get(id);
         if (ticket == null) {
@@ -74,21 +103,25 @@ public class TicketHub {
     }
 
     private String getTenant() {
-        return getSessionAttributes().get("tenant");
+        return (String) getSessionAttributes().get("tenant");
     }
 
     private String getSubject() {
-        return getSessionAttributes().get("subject");
+        return (String) getSessionAttributes().get("subject");
     }
 
-    Map<String, String> getSessionAttributes() {
+    Map<String, Object> getSessionAttributes() {
+        return getSessionAttributes(request);
+    }
+
+    static Map<String, Object> getSessionAttributes(HttpServletRequest request) {
         var cookies = request.getCookies();
         return Arrays.stream(cookies != null ? cookies : new Cookie[]{})
                 .filter(cookie -> "user".equals(cookie.getName()))
                 .findAny()
                 .map(cookie -> {
                     var components = cookie.getValue().split("\\s*/\\s*", 2);
-                    Map<String, String> map = new HashMap<>();
+                    Map<String, Object> map = new HashMap<>();
                     if (components.length > 0) {
                         map.put("tenant", components[0].trim());
                     }
@@ -108,6 +141,11 @@ public class TicketHub {
         var apiHolder = root.addServlet(ServletContainer.class, "/api/*");
         apiHolder.setInitOrder(0);
         apiHolder.setInitParameter("jersey.config.server.provider.packages", TicketHub.class.getPackageName());
+
+        var inputTransformer = new TenantInputTransformer();
+        root.addServlet(new ServletHolder(new ProxyServlet<>(styraRun, sessionManager, inputTransformer)), "/api/authz");
+
+        RbacServletHelper.addRbacServlets(root, "/api/rbac", styraRun, sessionManager, null);
 
         server.start();
         server.join();
