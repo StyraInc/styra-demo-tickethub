0133599970116b889a9b0787844cce0852ceb839
Johan Fylling <johan.dev@fylling.se>
2022-12-09 11:59:12 +0100

    Moving `TicketStatus` and `Tickets`
    Signed-off-by: Johan Fylling <johan.dev@fylling.se>

--- server/java/src/main/java/com/styra/tickethub/TicketHub.java
+++ server/java/src/main/java/com/styra/tickethub/TicketHub.java
@@ -23,12 +23,6 @@ import java.util.Map;
 
 @Path("/")
 public class TicketHub {
-    private record TicketStatus(boolean resolved) {
-    }
-
-    private record Tickets(List<Ticket> tickets) {
-    }
-
     private static final Storage storage = Storage.create();
 
     private @Context
@@ -111,5 +105,11 @@ public class TicketHub {
 
         server.start();
         server.join();
+    }
+
+    private record TicketStatus(boolean resolved) {
+    }
+
+    private record Tickets(List<Ticket> tickets) {
     }
 }
