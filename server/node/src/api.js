4c9d1efa4d5ac91fb460c17f1087d5b921a0532c
Stephan Renatus <stephan@styra.com>
2024-03-26 13:22:13 +0100

    server/node: apply auto-formatting
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- server/node/src/api.js
+++ server/node/src/api.js
@@ -1,46 +1,47 @@
-import 'express-async-errors' // makes sure uncaught errors in async handlers doesn't cause crash
-import StatusCodes from 'http-status-codes'
-import {Router} from 'express'
-import {tickets as ticketsDb, users as usersDb} from './db.js'
+import "express-async-errors"; // makes sure uncaught errors in async handlers doesn't cause crash
+import StatusCodes from "http-status-codes";
+import { Router } from "express";
+import { tickets as ticketsDb } from "./db.js";
 
 // all routes in this router is prefixed with /api, see ./server.js:42
-export const router = Router()
-const {OK, FORBIDDEN} = StatusCodes
+export const router = Router();
+const { OK } = StatusCodes;
 
 // resolve ticket
-router.post('/tickets/:id/resolve', async (req, res) => {
-  const ticket = ticketsDb[req.auth.tenant][req.params.id]
-  ticket.resolved = req.body.resolved ? true : false
-  ticket.last_updated = now()
-  return res.status(OK).json(asTicket(req.params.id, ticket))
-})
+router.post("/tickets/:id/resolve", async (req, res) => {
+  const ticket = ticketsDb[req.auth.tenant][req.params.id];
+  ticket.resolved = req.body.resolved ? true : false;
+  ticket.last_updated = now();
+  return res.status(OK).json(asTicket(req.params.id, ticket));
+});
 
 // create ticket
-router.post('/tickets', async (req, res) => {
-  const id = ticketsDb[req.auth.tenant].length
-  const ticket = req.body
-  ticket.last_updated = now()
-  ticketsDb[req.auth.tenant].push(ticket)
-  return res.status(OK).json(asTicket(id, ticket))
-})
+router.post("/tickets", async (req, res) => {
+  const id = ticketsDb[req.auth.tenant].length;
+  const ticket = req.body;
+  ticket.last_updated = now();
+  ticketsDb[req.auth.tenant].push(ticket);
+  return res.status(OK).json(asTicket(id, ticket));
+});
 
 // list all tickets
-router.get('/tickets', async (req, res) => {
-  const tickets = ticketsDb[req.auth.tenant]
-    .map((t, index) => asTicket(index, t))
-  return res.status(OK).json({tickets})
-})
+router.get("/tickets", async (req, res) => {
+  const tickets = ticketsDb[req.auth.tenant].map((t, index) =>
+    asTicket(index, t),
+  );
+  return res.status(OK).json({ tickets });
+});
 
 // get ticket
-router.get('/tickets/:id', async (req, res) => {
-  const ticket = ticketsDb[req.auth.tenant][req.params.id]
-  return res.status(OK).json(asTicket(req.params.id, ticket))
-})
+router.get("/tickets/:id", async (req, res) => {
+  const ticket = ticketsDb[req.auth.tenant][req.params.id];
+  return res.status(OK).json(asTicket(req.params.id, ticket));
+});
 
 function now() {
-  return new Date().toISOString().split('.')[0] + 'Z'
+  return new Date().toISOString().split(".")[0] + "Z";
 }
 
 function asTicket(id, attrs) {
-  return {id, ...attrs}
+  return { id, ...attrs };
 }
--- server/node/src/server.js
+++ server/node/src/server.js
@@ -1,46 +1,46 @@
-import 'express-async-errors' // makes sure uncaught errors in async handlers doesn't cause crash
-import 'module-alias/register.js'
+import "express-async-errors"; // makes sure uncaught errors in async handlers doesn't cause crash
+import "module-alias/register.js";
 
-import cookieParser from 'cookie-parser'
-import express from 'express'
-import StatusCodes from 'http-status-codes'
+import cookieParser from "cookie-parser";
+import express from "express";
+import StatusCodes from "http-status-codes";
 
-import {router as apiRouter} from './api.js'
+import { router as apiRouter } from "./api.js";
 
-const port = process.env.SERVER_PORT || 4000
-const host = process.env.SERVER_HOST || 'localhost'
-const app = express()
+const port = process.env.SERVER_PORT || 4000;
+const host = process.env.SERVER_HOST || "localhost";
+const app = express();
 
 // common middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // error handling
-app.use((err, req, res, _) => {
-  console.error(err, true)
-  const status = (err instanceof HttpError ? err.HttpStatus : StatusCodes.BAD_REQUEST)
+app.use((err, _req, res, _next) => {
+  const status =
+    err instanceof HttpError ? err.HttpStatus : StatusCodes.BAD_REQUEST;
   return res.status(status).json({
-    error: err && err.message,
-  })
-})
+    error: err?.message,
+  });
+});
 
 // authentication
 app.use((req, res, next) => {
-  if ('user' in req.cookies) {
-    const [tenant, subject] = req.cookies.user.split(' / ')
-    req.auth = {tenant, subject}
-    next()
+  if ("user" in req.cookies) {
+    const [tenant, subject] = req.cookies.user.split(" / ");
+    req.auth = { tenant, subject };
+    next();
   } else {
     res.status(StatusCodes.UNAUTHORIZED).json({
-      "error": "authentication error: user credentials not provided"
-    })
+      error: "authentication error: user credentials not provided",
+    });
   }
-})
+});
 
 // API
-app.use('/api', apiRouter)
+app.use("/api", apiRouter);
 
 app.listen(port, host, () => {
-  console.info(`Server started on port: ${port}`)
-})
+  console.info(`Server started on port: ${port}`);
+});
