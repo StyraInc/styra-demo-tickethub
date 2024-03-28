4f9a46a5f5858b7b748d01c2d688c1beae406748
Charles A. Daniels <charles@styra.com>
2024-03-28 12:53:13 -0500

    WiP: build opa-java SDK with container

--- server/java/docker_entrypoint.sh
+++ server/java/docker_entrypoint.sh
@@ -7,6 +7,7 @@ set -u
 set -x
 
 cp -R /mnt/tickethub-java/* /src/tickethub-java/
+cp /src/opa-java/build/libs/api.jar /src/tickethub-java/libs
 cd /src/tickethub-java
 ./gradlew clean
 ./gradlew run
