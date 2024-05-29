ca8206d6dadeeec74d1e6b8a6ec339043a48783a
Stephan Renatus <stephan@styra.com>
2024-05-29 13:52:44 +0200

    opa-react: adapt useAuthz() to single-request mode
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- client/react/packages/opa-react/src/use-authz.ts
+++ client/react/packages/opa-react/src/use-authz.ts
@@ -1,66 +1,86 @@
-import { useContext, useMemo } from "react";
+import { useContext, useCallback, useEffect, useState } from "react";
 import { AuthzContext } from "./opa-provider";
 import { Input, Result } from "@styra/opa";
 import merge from "lodash/merge";
+import { useDeepCompareMemoize } from "use-deep-compare-effect";
 
-export interface UseAuthzResult {
-  result: Result | undefined;
-  isLoading: boolean;
-}
+export type UseAuthzResult<T> =
+  | { isLoading: true; result: undefined; error: undefined }
+  | { isLoading: false; result: T; error: undefined }
+  | { isLoading: false; result: undefined; error: Error };
 
 /**
  * This hook provides more flexibility than the <Authz/> component, allowing you to
  * work with authorization decisions directly in your code.
- * You can:
- * - preload authorization requests
- * - determine which requests are still loading
- * - examine results of individual resource authorization decisions
-
- * @param  resources - Array of items to check for user's authorization.
- * @return Authorization query results and loading status. The decision array will be in
- * the same order as the input array, allowing you to easily match up results to inputs.
  *
- * @example
- * To see this in action, copy to its own file, add necessary imports,
- * then insert a <DemoButtonBar /> somewhere in the code.
- *
- * export default function DemoButtonBar() {
- *   const [authzResources] = useState([
- *     types.READ_WORKSPACE.resource(),
- *     SystemsTypes[Kind.SYSTEMS].CREATE_SYSTEM.resource(),
- *   ] as Resource[])
- *
- *   const { isLoading, decision } = useAuthz(authzResources)
- *
- *   if (isLoading) {
- *     return null
- *   }
- *
- *   const [isReadWorkspaceAllowed, isCreateSystemAllowed] = decision
- *   return (
- *     <div style={{ backgroundColor: 'red' }}>
- *       {isReadWorkspaceAllowed && <button>Read</button>}{' '}
- *       <button disabled={!isCreateSystemAllowed}>Create</button>
- *     </div>
- *   )
- * }
+ * TODO(sr): update docs
  */
-export default function useAuthz(path?: string, input?: Input): UseAuthzResult {
+export default function useAuthz(
+  path?: string,
+  input?: Input,
+): UseAuthzResult<Result> {
   const context = useContext(AuthzContext);
   if (context === null) {
     throw Error("Authz/useAuthz can only be used inside an AuthzProvider");
   }
   const { sdk, defaultPath, defaultInput } = context;
 
-  return useMemo<UseAuthzResult>(() => {
-    const p = path ?? defaultPath;
-    const i = mergeInput(input, defaultInput);
-    const result = sdk.evaluate<Input, Result>(p, i);
-    return {
-      result,
-      isLoading: result === undefined,
+  const [isLoading, setIsLoading] = useState(true);
+  const [result, setResult] = useState<Result>();
+  const [error, setError] = useState<Error>();
+
+  // TODO(sr): This feels wrong. Figure out just how wrong it is.
+  const requestMemo = useDeepCompareMemoize({
+    defaultPath,
+    defaultInput,
+    input,
+    path,
+  });
+
+  const evaluate = useCallback<(signal: AbortSignal) => Promise<Result>>(
+    async (_: AbortSignal): Promise<Result> => {
+      // TODO(sr): our SDK doesn't do signal handling...
+      const { defaultPath, defaultInput, input, path } = requestMemo;
+      const p = path ?? defaultPath;
+      const i = mergeInput(input, defaultInput);
+      return p ? sdk.evaluate(p, i) : sdk.evaluateDefault(i);
+    },
+    [sdk, requestMemo],
+  );
+
+  useEffect(() => {
+    setIsLoading(true);
+    setResult(undefined);
+    setError(undefined);
+
+    const abortController = new AbortController();
+
+    evaluate(abortController.signal)
+      .then((data) => {
+        if (!abortController.signal.aborted) {
+          setIsLoading(false);
+          setResult(data);
+          setError(undefined);
+        }
+      })
+      .catch((error: unknown) => {
+        if (!abortController.signal.aborted) {
+          setIsLoading(false);
+          setResult(undefined);
+          setError(
+            error instanceof Error
+              ? error
+              : new Error("An unexpected error occurred", { cause: error }),
+          );
+        }
+      });
+
+    return (): void => {
+      abortController.abort();
     };
-  }, [sdk, path, input, defaultPath, defaultInput]);
+  }, [evaluate]);
+
+  return { isLoading, result, error } as UseAuthzResult<Result>;
 }
 
 function mergeInput(input?: Input, defaultInput?: Input): Input | undefined {
