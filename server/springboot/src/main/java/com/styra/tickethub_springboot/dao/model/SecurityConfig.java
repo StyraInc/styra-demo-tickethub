df97a9ac1d6f731f379482f987ba679deb9d1273
Charles Daniels <charles@styra.com>
2024-06-17 14:35:04 -0400

    disable CSRF to fix custom auth manager

--- server/springboot/src/main/java/com/styra/tickethub_springboot/dao/model/SecurityConfig.java
+++ server/springboot/src/main/java/com/styra/tickethub_springboot/dao/model/SecurityConfig.java
@@ -38,8 +38,14 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // NOTE: The `.csrf().disable()` disables CSRF protections. This could
+        // be a serious security vulnerability in a production environment.
+        // However, since this API is intended for educational and development
+        // purposes, it is disabled because it makes it easier to work with
+        // locally. If you want to use any of this code for a production
+        // service, it is important to re-enable CSRF protection.
         http.authorizeHttpRequests(authorize -> authorize
-                .anyRequest().access(customAuthManager()));
+                .anyRequest().access(customAuthManager())).csrf().disable();
 
         return http.build();
     }
