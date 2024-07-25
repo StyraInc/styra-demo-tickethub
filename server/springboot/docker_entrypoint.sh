3058ddfaf88d766684e0c2ee4d6e9d139baf0255
Charles Daniels <charles@styra.com>
2024-07-25 14:34:33 -0400

    switch to using opa-springboot from Maven Central

--- server/springboot/docker_entrypoint.sh
+++ server/springboot/docker_entrypoint.sh
@@ -6,12 +6,5 @@ set -e
 set -u
 set -x
 
-# TODO: for local dev only
-cd /src/opa-springboot
-cp -R /mnt/opa-springboot/* ./
-./gradlew publishToMavenLocal -Pskip.signing
-
-find ~/.m2/repository
-
 cd /src/tickethub-springboot
 ./gradlew run -x test -x testClasses
