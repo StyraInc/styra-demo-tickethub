144306f31404db3574df74621fae1a13ef27a193
Philip Conrad <philip@chariot-chaser.net>
2024-08-28 13:54:18 -0400

    server/csharp: Update to use logging + authz attribute. (#295)
    This commit includes 3x changes:
    
     - We now properly tie in the OpaClient's logging into the ASP.NET
       logging system.
     - We properly implement the authz controls on the endpoints with an
       AuthorizationFilter attribute.
     - Some unused imports have been removed.
    
    The original implementation for filtering bad requests was done as an
    ActionFilterAttribute, which was more powerful than we needed, and runs
    later in the controller filtering process.
    
    Signed-off-by: Philip Conrad <philip@chariot-chaser.net>

--- server/csharp/TicketHub/Authorization/OpaAuthorizationAttribute.cs
+++ server/csharp/TicketHub/Authorization/OpaAuthorizationAttribute.cs
@@ -5,14 +5,65 @@ using Styra.Opa;
 
 namespace TicketHub.Authorization;
 
-// Custom authorization attribute, allowing us to template in the fields our
-// policy needs in the input.
-public class OpaAuthorizationAttribute : ActionFilterAttribute
+// Custom authorization attribute, implemented as an authorization filter.
+// This design allows us to template in the fields our policy needs
+// in the input.
+public class OpaRuleAuthorizationAttribute : Attribute, IAsyncAuthorizationFilter
 {
     private readonly string _path;
     private readonly string _action;
 
-    public OpaAuthorizationAttribute(string path, string action)
+    public OpaRuleAuthorizationAttribute(string path, string action)
+    {
+        _path = path;
+        _action = action;
+    }
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        bool authorized = await IsAuthorized(context);
+        if (!authorized)
+        {
+            // If not authorized, return an error response
+            context.Result = new ContentResult
+            {
+                Content = "Not Authorized",
+                StatusCode = 403,
+            };
+            return;
+        }
+
+        // If authorized, allow normal controller handling.
+        return;
+    }
+
+    private async Task<bool> IsAuthorized(AuthorizationFilterContext context)
+    {
+        var httpContext = context.HttpContext;
+        string tenant = httpContext.Items["Tenant"]?.ToString() ?? "";
+        string subject = httpContext.Items["Subject"]?.ToString() ?? "";
+        var authzService = context.HttpContext.RequestServices.GetRequiredService<OpaAuthzService>();
+        OpaClient opa = authzService.GetClient();
+
+        bool allowed = await opa.evaluate<bool>(_path, new Dictionary<string, object>(){
+            { "tenant", tenant },
+            { "user", subject },
+            { "action", _action },
+        });
+        return allowed;
+    }
+}
+
+
+// Custom authorization attribute, implemented as an action filter.
+// This design allows us to template in the fields our policy needs
+// in the input.
+public class OpaRuleActionFilterAttribute : ActionFilterAttribute
+{
+    private readonly string _path;
+    private readonly string _action;
+
+    public OpaRuleActionFilterAttribute(string path, string action)
     {
         _path = path;
         _action = action;
