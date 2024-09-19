edbeaf02531bad3d980c2c9044ed54acfef072b1
Charles Daniels <charles@styra.com>
2024-09-19 15:21:34 -0400

    remove experimental SB changes

--- server/springboot/docker_entrypoint.sh
+++ server/springboot/docker_entrypoint.sh
@@ -6,13 +6,5 @@ set -e
 set -u
 set -x
 
-# TODO: for local dev only
-cd /src/opa-springboot
-cp -R /mnt/opa-springboot/* ./
-./gradlew publishToMavenLocal -Pskip.signing
-
-find ~/.m2/repository
-
 cd /src/tickethub-springboot
-./gradlew clean -x test -x testClasses
 ./gradlew run -x test -x testClasses
--- server/springboot/src/main/java/com/styra/tickethub_springboot/dao/model/SecurityConfig.java
+++ server/springboot/src/main/java/com/styra/tickethub_springboot/dao/model/SecurityConfig.java
@@ -16,8 +16,6 @@ import org.springframework.security.web.access.intercept.RequestAuthorizationCon
 import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import com.styra.opa.springboot.OPAAuthorizationManager;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.access.AccessDeniedHandler;
 
 import com.styra.opa.OPAClient;
 
@@ -26,8 +24,6 @@ import static java.util.Map.entry;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Supplier;
-
 
 @Configuration
 @EnableWebSecurity
@@ -54,26 +50,17 @@ public class SecurityConfig {
 
         AuthorizationManager<RequestAuthorizationContext> am = new OPAAuthorizationManager(opa, "tickets/spring/main");
 
-        CustomAccessDeniedHandler adh = new CustomAccessDeniedHandler();
-        adh.setManager(am);
-
         // NOTE: The `.csrf(...)` disables CSRF protections. This could
         // be a serious security vulnerability in a production environment.
         // However, since this API is intended for educational and development
         // purposes, it is disabled because it makes it easier to work with
         // locally. If you want to use any of this code for a production
         // service, it is important to re-enable CSRF protection.
-        http.addFilterBefore(new CustomAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-            .authorizeHttpRequests(authorize -> authorize.anyRequest().access(am))
-            .csrf(csrf -> csrf.disable())
-            .exceptionHandling().accessDeniedHandler(adh);
+        http.authorizeHttpRequests(authorize -> authorize
+                .anyRequest().access(am)).csrf(csrf -> csrf.disable());
 
         return http.build();
     }
 
-    //@Bean
-    //public AccessDeniedHandler accessDeniedHandler(){
-    //    return new CustomAccessDeniedHandler();
-    //}
 
 }
