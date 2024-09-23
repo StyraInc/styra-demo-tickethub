b16825a2f043d7d721702b9c14e189201a5bc827
Charles Daniels <charles@styra.com>
2024-09-23 16:35:37 -0400

    pull in apring authN from charles/eopa-350

--- server/springboot/src/main/java/com/styra/tickethub_springboot/dao/model/SecurityConfig.java
+++ server/springboot/src/main/java/com/styra/tickethub_springboot/dao/model/SecurityConfig.java
@@ -2,20 +2,21 @@ package com.styra.tickethub_springboot.dao.model;
 
 import java.util.Random;
 
-import org.springframework.context.annotation.Bean;
+import com.styra.opa.springboot.OPAAuthorizationManager;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.slf4j.MDC;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import com.styra.opa.springboot.OPAAuthorizationManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 
 import com.styra.opa.OPAClient;
 
@@ -56,8 +57,9 @@ public class SecurityConfig {
         // purposes, it is disabled because it makes it easier to work with
         // locally. If you want to use any of this code for a production
         // service, it is important to re-enable CSRF protection.
-        http.authorizeHttpRequests(authorize -> authorize
-                .anyRequest().access(am)).csrf(csrf -> csrf.disable());
+        http.addFilterBefore(new CustomAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().access(am))
+            .csrf(csrf -> csrf.disable());
 
         return http.build();
     }
