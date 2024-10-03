f6f0b84eac9eb331332e545611b794f0d9f295c8
Philip Conrad <philip@chariot-chaser.net>
2024-10-03 01:25:31 -0400

    server/aspnetcore: Add ASP.NET Core SDK server.
    Signed-off-by: Philip Conrad <philip@chariot-chaser.net>

---
+++ server/aspnetcore/README.md
@@ -0,0 +1,17 @@
+# TicketHub C# Server
+
+The TicketHub sample app server component.
+
+## How to run
+
+1. `make run`.
+2. The server opens a listener on `0.0.0.0:4000`.
+
+## Notes
+
+This project was initially created with `dotnet new webapi -controllers --no-https --no-openapi --name TicketHub`, and modified from there.
+
+## SDKs
+
+* [SDK Documentation](https://docs.styra.com/sdk)
+* [OPA C# SDK](https://github.com/StyraInc/opa-csharp)
---
+++ server/aspnetcore/TicketHub/Database/PostgresContext.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TicketHub.Database;
+
+public partial class PostgresContext : DbContext
+{
+    public PostgresContext()
+    {
+    }
+
+    public PostgresContext(DbContextOptions<PostgresContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Tenant> Tenants { get; set; }
+
+    public virtual DbSet<Ticket> Tickets { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string pgConnString = Environment.GetEnvironmentVariable("DATABASE_CONN_STR") ?? "Host=localhost;Database=postgres;Username=postgres;Password=schmickethub";
+        optionsBuilder.UseNpgsql(pgConnString, npgsqlOptions => npgsqlOptions.UseNodaTime());
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Customers_pkey");
+
+            entity.HasIndex(e => new { e.Tenant, e.Name }, "Customers_tenant_name_key").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.Tenant).HasColumnName("tenant");
+
+            entity.HasOne(d => d.TenantNavigation).WithMany(p => p.Customers)
+                .HasForeignKey(d => d.Tenant)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Customers_tenant_fkey");
+        });
+
+        modelBuilder.Entity<Tenant>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Tenants_pkey");
+
+            entity.HasIndex(e => e.Name, "Tenants_name_key").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Ticket>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Tickets_pkey");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Customer).HasColumnName("customer");
+            entity.Property(e => e.Description).HasColumnName("description");
+            entity.Property(e => e.LastUpdated)
+                .HasDefaultValueSql("now()")
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("last_updated");
+            entity.Property(e => e.Resolved)
+                .HasDefaultValue(false)
+                .HasColumnName("resolved");
+            entity.Property(e => e.Tenant).HasColumnName("tenant");
+
+            entity.HasOne(d => d.CustomerNavigation).WithMany(p => p.Tickets)
+                .HasForeignKey(d => d.Customer)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Tickets_customer_fkey");
+
+            entity.HasOne(d => d.TenantNavigation).WithMany(p => p.Tickets)
+                .HasForeignKey(d => d.Tenant)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Tickets_tenant_fkey");
+
+            // Try manually designating the Navigation properties.
+            entity.Navigation(e => e.CustomerNavigation);
+            entity.Navigation(e => e.TenantNavigation);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
