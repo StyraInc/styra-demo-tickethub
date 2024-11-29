26a44952809fea9dec56712d3b1c391472676fa9
Stephan Renatus <stephan@styra.com>
2024-11-29 14:11:51 +0100

    ci: enable conditions PW tests for C#, add wait-for-assign
    Without the wait, the test would fail with the C# backend.
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- tests/e2e/tests/tickets.spec.ts
+++ tests/e2e/tests/tickets.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-const baseURL = `http://127.0.0.1:3000/?batch=${process.env.BATCHING == "true"}`;
+const baseURL = `http://127.0.0.1:3000/?batch=${process.env.BATCHING === "true"}`;
+const conditionsDisabled = process.env.CONDITIONS !== "true";
+const reasonsDisabled = process.env.REASONS !== "true";
 
 test("has title with tenant and list of tickets", async ({ page }) => {
   await page.goto(baseURL);
@@ -34,7 +36,7 @@ test("select another tenant's user switches title and ticket list", async ({
 });
 
 test.describe("data filtering using conditions", () => {
-  test.skip(process.env.CONDITIONS != "true", "skipping conditions test");
+  test.skip(conditionsDisabled, "skipping conditions test");
 
   const unassignTicket5 = async ({ page }) => {
     await page.goto(baseURL);
@@ -45,22 +47,29 @@ test.describe("data filtering using conditions", () => {
   test.beforeEach(unassignTicket5);
   test.afterEach(unassignTicket5);
 
-  test("ceasar can only see unresolved tickets and those assigned to them", async ({ page }) => {
+  test("ceasar can only see unresolved tickets and those assigned to them", async ({
+    page,
+  }) => {
     await page.goto(baseURL);
     await page.getByLabel("User").selectOption("ceasar");
     await expect(page.locator("#ticket-list > tbody > tr ")).toHaveCount(5);
 
     await page.getByLabel("User").selectOption("alice");
+    const assign = page.waitForResponse(
+      (response) =>
+        response.url().endsWith("assign") &&
+        response.request().method() === "POST",
+    );
     await page.locator("#ticket-5 select").selectOption("Bob");
+    await assign;
 
     await page.getByLabel("User").selectOption("ceasar");
     await expect(page.locator("#ticket-list > tbody > tr ")).toHaveCount(4);
   });
 });
 
-
 test.describe("showing reasons for denials", () => {
-  test.skip(process.env.REASONS != "true", "skipping reasons test");
+  test.skip(reasonsDisabled, "skipping reasons test");
 
   const unresolveTicket1 = async ({ page }) => {
     await page.goto(baseURL);
@@ -68,7 +77,7 @@ test.describe("showing reasons for denials", () => {
     await page.getByLabel("User").selectOption("alice");
 
     const resolved = await page.locator("#resolved").textContent();
-    if (resolved == "yes") {
+    if (resolved === "yes") {
       await page.getByRole("button", { name: "Resolve" }).click();
     }
   };
@@ -76,14 +85,20 @@ test.describe("showing reasons for denials", () => {
   test.beforeEach(unresolveTicket1);
   test.afterEach(unresolveTicket1);
 
-  test("bob gets an error message when trying to resolve the ticket", async ({ page }) => {
+  test("bob gets an error message when trying to resolve the ticket", async ({
+    page,
+  }) => {
     await page.goto(baseURL);
     await page.locator("#ticket-5 td:nth-child(1)").click();
     await page.getByLabel("User").selectOption("bob");
     const resolveButton = page.getByRole("button", { name: "Resolve" });
-    await resolveButton.evaluate(element => element.removeAttribute("disabled"));
+    await resolveButton.evaluate((element) =>
+      element.removeAttribute("disabled"),
+    );
 
     await resolveButton.click();
-    await expect(page.locator(".update-status")).toContainText("resolver role is required to resolve");
+    await expect(page.locator(".update-status")).toContainText(
+      "resolver role is required to resolve",
+    );
   });
 });
